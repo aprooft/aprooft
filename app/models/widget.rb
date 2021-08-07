5d9431bf4f48c6ed868ec9f3d251b39df183134e
Piotr Polanski <polanski.pio@gmail.com>
2021-08-07 16:20:03 +0900

    added validations to current models

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,3 +1,8 @@
 class Widget < ApplicationRecord
   belongs_to :user
+  has_many :youtubes, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :product_title, presence: true
+  validates :product_pic, presence: true
 end
