4adfee46afccec0e1dfea909236adb038d7a570e
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-08-07 12:51:32 +0900

    add widget model

---
+++ db/migrate/20210807035019_create_widgets.rb
@@ -0,0 +1,12 @@
+class CreateWidgets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :widgets do |t|
+      t.references :user, null: false, foreign_key: true
+      t.text :product_title
+      t.string :product_pic
+      t.jsonb :style
+
+      t.timestamps
+    end
+  end
+end
