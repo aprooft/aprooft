b984b27cd82d6de77563aeb08b4f839d273fe541
Piotr Polanski <polanski.pio@gmail.com>
2021-08-07 15:49:13 +0900

    added youtube_id extract method

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,2 +1,9 @@
 class WidgetsController < ApplicationController
+  private
+
+  def youtube_id(youtube_url)
+    regex = %r{(?:youtube(?:-nocookie)?\.com/(?:[^/\n\s]+/\S+/|(?:v|e(?:mbed)?)/|\S*?[?&]v=)|youtu\.be/)([a-zA-Z0-9_-]{11})}
+    match = regex.match(youtube_url)
+    match[1] if match && !match[1].blank?
+  end
 end
