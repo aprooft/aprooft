bb510fcb9646406012a7e4c19020af22d78a3baa
Piotr Polanski <polanski.pio@gmail.com>
2021-08-10 22:16:14 +0900

    added basic animations

--- app/javascript/components/sidebar.js
+++ app/javascript/components/sidebar.js
@@ -1,15 +1,28 @@
 const sideBar = () => {
   const btn = document.getElementById("btn");
-  const searchBtn = document.querySelector(".bx-search");
   const sidebar = document.querySelector('.sidebar');
+  const stylesBtn = document.getElementById('btn-styles');
+  const styleBar = document.querySelector(".stylebar-wrapper");
+  const layout = document.querySelector(".layout");
+  const styleOptionsWrapper = document.querySelector(".style-options-wrapper");
+  const styleSectionName = document.querySelector(".style-section-name");
 
   btn.addEventListener("click", () => {
-    sidebar.classList.toggle("active")
+    sidebar.classList.toggle("active");
+    if (styleBar.classList.contains("active")) {
+      styleBar.classList.remove("active");
+    }
   })
 
-    .searchBtn.addEventListener("click", () => {
-      sidebar.classList.toggle("active")
-    })
+  stylesBtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+    styleBar.classList.toggle("active");
+  })
+
+  layout.addEventListener("click", () => {
+    styleOptionsWrapper.classList.add("active");
+    styleSectionName.innerText = "Layout"
+  })
 }
 
 export { sideBar };
