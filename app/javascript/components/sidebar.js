448156867923b1eebe2ddcad33a8ea0cd15605ea
Piotr Polanski <polanski.pio@gmail.com>
2021-08-11 23:35:45 +0900

    js in progress

--- app/javascript/components/sidebar.js
+++ app/javascript/components/sidebar.js
@@ -3,19 +3,20 @@ const sideBar = () => {
   const sidebar = document.querySelector('.sidebar');
   const stylesBtn = document.getElementById('btn-styles');
   const styleBar = document.querySelector(".stylebar-wrapper");
-  const layout = document.querySelector(".layout-style");
-  const size = document.querySelector(".size-style");
-  const font = document.querySelector(".font-style");
-  const colors = document.querySelector(".colors-style");
-  const background = document.querySelector(".background-style");
+  const styles = document.querySelectorAll(".style");
+  const layout = document.querySelector("#layout-style");
+  const size = document.querySelector("#size-style");
+  const font = document.querySelector("#font-style");
+  const colors = document.querySelector("#colors-style");
+  const background = document.querySelector("#background-style");
   const styleOptionsWrapper = document.querySelector(".style-options-wrapper");
   const styleSectionName = document.querySelector(".style-section-name");
   const navBtns = document.querySelectorAll(".nav-btn");
 
-  const styleOptionsContent = document.querySelector(".style-options-content");
+  // const styleOptionsContent = document.querySelector(".style-options-content");
 
-  const fontSizeSlider = document.getElementById("font-size-slider");
-  const fontSizeValue = document.getElementById("font-size-value");
+  // const fontSizeSlider = document.getElementById("font-size-slider");
+  // const fontSizeValue = document.getElementById("font-size-value");
 
   // fontSizeSlider.addEventListener("input", e => {
   //   // demoText.style.fontSize = `${e.target.value}px`;
@@ -24,7 +25,14 @@ const sideBar = () => {
 
   navBtns.forEach((navBtn) => {
     navBtn.addEventListener("click", () => {
-      navBtn.classList.toggle("focus");
+      sidebar.classList.toggle("active");
+      if (styleBar.classList.contains("active")) {
+        styleBar.classList.remove("active");
+      }
+      navBtns.forEach((navBtn) => {
+        navBtn.classList.remove("focus");
+      })
+      navBtn.classList.add("focus");
     })
   })
 
@@ -40,8 +48,17 @@ const sideBar = () => {
     styleBar.classList.toggle("active");
   })
 
+  styles.forEach((style) => {
+    style.addEventListener("click", (e) => {
+      styles.forEach((style) => {
+        style.classList.remove("focus");
+      })
+      e.currentTarget.classList.add("focus");
+      styleOptionsWrapper.classList.add("active");
+    })
+  })
+
   layout.addEventListener("click", () => {
-    styleOptionsWrapper.classList.add("active");
     styleSectionName.innerText = "Layout"
   })
 
