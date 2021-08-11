d757cd1b587d41632d2639c0376f30153eda7437
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-08-11 22:25:15 +0900

    create basic widget dev and top bar dev

--- app/javascript/packs/edit-widget.jsx
+++ app/javascript/packs/edit-widget.jsx
@@ -2,7 +2,14 @@ import { h, render } from "preact";
 import register from 'preact-custom-element'
 
 function EditWidget(){
-    return <p>Abc</p> 
+    return (
+        <div class="widget-dev">
+            <div class="topbar-dev">
+
+            </div>
+            <p>3</p>
+        </div>
+    );    
 }
 
 register(EditWidget, 'edit-widget', [])
