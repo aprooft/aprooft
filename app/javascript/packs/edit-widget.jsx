bd72fd76215dd939ea236597b147d44d599fa66a
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-08-11 22:54:12 +0900

    add youtube&reddit nav button

--- app/javascript/packs/edit-widget.jsx
+++ app/javascript/packs/edit-widget.jsx
@@ -5,7 +5,12 @@ function EditWidget(){
     return (
         <div class="widget-dev">
             <div class="topbar-dev">
-
+                <div class="nav-btn">
+                    <img src="https://img.icons8.com/material-outlined/55/ffffff/youtube--v1.png"/>                
+                </div>
+                <div class="nav-btn">
+                    <img src="https://img.icons8.com/ios-glyphs/55/ffffff/reddit.png"/>           
+                </div>
             </div>
             <p>3</p>
         </div>
