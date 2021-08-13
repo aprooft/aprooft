aa49dbe92047e276d16b4057afd5d593d5ae58e2
Piotr Polanski <polanski.pio@gmail.com>
2021-08-13 21:15:48 +0900

    color styles

--- app/javascript/components/widget_styling.js
+++ app/javascript/components/widget_styling.js
@@ -1,10 +1,7 @@
 const widgetStyling = () => {
   const widgetDev = document.querySelector(".widget-dev")
-  console.log(widgetDev)
-
 
   const sizeOptions = document.querySelectorAll(".size-option")
-  console.log(sizeOptions)
   sizeOptions.forEach((sizeOption) => {
     sizeOption.addEventListener("click", (e) => {
       switch (sizeOption.id) {
@@ -22,6 +19,32 @@ const widgetStyling = () => {
           break;
       }
     })
+  })
+
+  //colors section
+  const topbarColor = document.getElementById("top-bar-color");
+  const topbarDev = document.querySelector(".topbar-dev");
+  const highlightColor = document.getElementById("highlight-color");
+  const widgetNavBtnActive = document.querySelector(".widget-nav-btn.active");
+  console.log(widgetNavBtnActive)
+  topbarDev.style.backgroundColor = "#11101D"
+
+  topbarColor.addEventListener('change', () => {
+    topbarDev.style.backgroundColor = topbarColor.value;
+  })
+
+  highlightColor.addEventListener('change', () => {
+    widgetNavBtnActive.style.background =
+      `linear-gradient(0deg, ${topbarDev.style.backgroundColor.value} 30%, ${highlightColor.value} 90%)`
+    widgetNavBtnActive.style.borderBottom = `10px solid ${highlightColor.value}`
+  })
+
+  // background section
+  const IroColorPicker = document.querySelector(".IroColorPicker");
+  const hexInput = document.getElementById("hexInput");
+
+  IroColorPicker.addEventListener('click', (e) => {
+    widgetDev.style.backgroundColor = `${hexInput.value}`;
   })
 
 }
