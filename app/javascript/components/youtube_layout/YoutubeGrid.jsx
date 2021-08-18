87e4510e8c6a23e38b115e37d492fc31cffcbc9a
Piotr Polanski <polanski.pio@gmail.com>
2021-08-18 20:54:18 +0900

    grid styles base

--- app/javascript/components/youtube_layout/YoutubeGrid.jsx
+++ app/javascript/components/youtube_layout/YoutubeGrid.jsx
@@ -2,12 +2,35 @@ import { h, render, Fragment } from "preact";
 
 export default function YoutubeGrid(props) {
     const data = props.data
-    
+
     // TODO
     return (
-        <div>  
-            <p>Grid</p>
-            <p>{data.title}</p>
-        </div>     
+        <div class="video-card-grid">
+            <div class="video-card-grid-thumbnail">
+                <div class="grid-info-thumbnail">
+                    <img src={data.thumbnail} />
+                </div>
+                <div class="grid-info-playicon">
+                    <i class="fas fa-play-circle"></i>
+                </div>
+            </div>
+            <div class="video-card-grid-info">
+                <div class="grid-info-title">
+                    <div class="grid-info-title-insidebox">
+                        {data.title}
+                    </div>
+                </div>
+                <div class="grid-info-details">
+                    <div class="grid-description">
+                        {data.description}
+                    </div>
+                    <div class="grid-info-number">
+                        <span><i class="fas fa-eye"></i> {data.view_count}</span>
+                        <span><i class="fas fa-thumbs-up"></i> {data.like_count}</span>
+                        <span><i class="fas fa-thumbs-down"></i> {data.dislike_count}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
