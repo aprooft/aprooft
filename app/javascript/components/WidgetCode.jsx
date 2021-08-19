5414c151692d51fdc2f19cb3048aad8f2be5a3b2
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-08-20 01:39:12 +0900

    setup prism to styling code

--- app/javascript/components/WidgetCode.jsx
+++ app/javascript/components/WidgetCode.jsx
@@ -1,11 +1,10 @@
-import { h, render, Fragment } from "preact";
+import { h, render } from "preact";
+import Prism from 'prismjs';
+import PrismJSX from "prismjs/components/prism-jsx";
+import "prismjs/themes/prism-solarizedlight.css";
 
 export default function WidgetCode(props) {
-    return (
-        <>    
-            <div class="widget-dev" style="display:flex; justify-content:center;align-items:center"> 
-                <pre>
-                    {`<script>
+    const code = `<script>
     ((e, l, i, x, y, b, z) => {
     e[y]=e[y]||function(){(e[y].q=e[y].q||[]).push(arguments)};
     b=l.createElement(i);z=l.getElementsByTagName(i)[0];b.async=1;
@@ -13,9 +12,15 @@ export default function WidgetCode(props) {
     })(window, document, 'script', 'https://aprooft.com/widget.js', 'aprooft');
 
     aprooft('https://aprooft.com', '${props.widgetId}');
-</script>`}
-                </pre> 
-            </div>   
-        </>    
+</script>`;
+
+    let highlighted = Prism.highlight(code, Prism.languages.html, "html");
+
+    return (
+        <div class="widget-code-box"> 
+            <pre>
+                <code dangerouslySetInnerHTML={{ __html: highlighted }} />
+            </pre>
+        </div>
     )
 }    
