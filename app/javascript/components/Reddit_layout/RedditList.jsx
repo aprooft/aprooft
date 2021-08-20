1f1969a7c15167ae398e099e2cd16eac3805fa13
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-08-20 22:17:13 +0900

    add basic reddit data

--- app/javascript/components/Reddit_layout/RedditList.jsx
+++ app/javascript/components/Reddit_layout/RedditList.jsx
@@ -8,6 +8,14 @@ export default function RedditGrid(props) {
         <div>
             { data.map( redditdata => 
                 <div class="reddit-card-list">
+                    <div class="reddit-card-list-left">
+                        <ThumbsUp />
+                        <p>{redditdata.ups}</p>
+                        <ThumbsDown />
+                    </div>
+                    <div class="reddit-card-list-right"></div>
+
+                    
                     <p>{redditdata.thread_id}</p>
                 </div> 
             ) }
