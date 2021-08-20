c2a081f0ca8fa549dec54aeb1dc29a9445a10580
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-08-20 18:37:03 +0900

    save reddit url to db

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -49,11 +49,17 @@ class WidgetsController < ApplicationController
   end
 
   def update
-    urls = params["youtube-link"].reject{ |link| link=="" }
-    urls.each do |link|
+    yturls = params["youtube-link"].reject{ |link| link=="" }
+    rdurls = params["reddit-link"].reject{ |link| link=="" }
+    yturls.each do |link|
       youtube = Youtube.new(fetchYoutubeApi(link))
       youtube.widget = @widget
       youtube.save
+    end
+    rdurls.each do |link|
+      reddit = Reddit.new(fetchRedditApi(link))
+      reddit.widget = @widget
+      reddit.save
     end
     redirect_to edit_widget_path(@widget)
   end
