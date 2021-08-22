6c093f5b6f2eb59e5448a8923ccdb94aef303090
Piotr Polanski <polanski.pio@gmail.com>
2021-08-22 22:40:28 +0900

    added links to reddit and youtube previews

--- app/javascript/components/youtube_layout/YoutubeGrid.jsx
+++ app/javascript/components/youtube_layout/YoutubeGrid.jsx
@@ -8,31 +8,32 @@ export default function YoutubeGrid(props) {
     return (
         <div class="youtube-grid-wrapper">
             {data.map(youtubedata =>
-                <div class="video-card-grid">
-                    <div class="video-card-grid-thumbnail">
-                        <div class="grid-info-thumbnail">
-                            <img src={youtubedata.thumbnail} />
-                        </div>
-                        <div class="grid-info-playicon">
-                            {/* <i class="fas fa-play-circle"></i> */}
-                            <Youtube size={48} opacity={0.85} />
-                        </div>
-                    </div>
-                    <div class="video-card-grid-info">
-                        <div class="grid-info-title">
-                            <div class="grid-info-title-insidebox">
-                                {youtubedata.title}
+                <a href={`https://www.youtube.com/watch?v=${youtubedata.video_id}`} target='_blank'>
+                    <div class="video-card-grid">
+                        <div class="video-card-grid-thumbnail">
+                            <div class="grid-info-thumbnail">
+                                <img src={youtubedata.thumbnail} />
+                            </div>
+                            <div class="grid-info-playicon">
+                                <Youtube size={48} opacity={0.85} />
                             </div>
                         </div>
-                        <div class="grid-info-details">
-                            <div class="grid-info-number">
-                                <span><Eye size={17} /> {youtubedata.view_count}</span>
-                                <span><ThumbsUp size={17} /> {youtubedata.like_count}</span>
-                                <span><ThumbsDown size={17} /> {youtubedata.dislike_count}</span>
+                        <div class="video-card-grid-info">
+                            <div class="grid-info-title">
+                                <div class="grid-info-title-insidebox">
+                                    {youtubedata.title}
+                                </div>
+                            </div>
+                            <div class="grid-info-details">
+                                <div class="grid-info-number">
+                                    <span><Eye size={17} /> {youtubedata.view_count}</span>
+                                    <span><ThumbsUp size={17} /> {youtubedata.like_count}</span>
+                                    <span><ThumbsDown size={17} /> {youtubedata.dislike_count}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </a>
             )}
         </div>
     )
--- app/javascript/components/youtube_layout/YoutubeMasonry.jsx
+++ app/javascript/components/youtube_layout/YoutubeMasonry.jsx
@@ -2,13 +2,15 @@ import { h, render, Fragment } from "preact";
 
 export default function YoutubeMasonry(props) {
     const data = props.data
-    
+
     // TODO
-    return (  
-        <div class="youtube-grid-masonry"> 
-            { data.map( youtubedata =>
-                <p>{youtubedata.title}</p>
+    return (
+        <div class="youtube-grid-masonry">
+            {data.map(youtubedata =>
+                <a href={`https://www.youtube.com/watch?v=${youtubedata.video_id}`} target='_blank'>
+                    <p>{youtubedata.title}</p>
+                </a>
             )}
-        </div>   
+        </div>
     )
 }
