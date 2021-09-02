ab384dd5057c1ebfb284a81eec9cc0da3201987c
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-09-02 20:34:59 +0900

    reload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,18 +26,6 @@ ActiveStorage.start()
 // External imports
 import "bootstrap";
 import AOS from 'aos'
-AOS.init({
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-});
-
-AOS.refresh();
 
 
 
@@ -53,6 +41,20 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   // Create a new color picker instance
   // https://iro.js.org/guide.html#getting-started
+
+  AOS.init({
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'load', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  });
+  
+  // AOS.refresh();
+  // Location.reload();
 
   var colorPicker = new iro.ColorPicker(".colorPicker", {
     // color picker options
@@ -82,6 +84,7 @@ document.addEventListener('turbolinks:load', () => {
     widgetStyling();
   }
   changeWidget();
+
 });
 
 
